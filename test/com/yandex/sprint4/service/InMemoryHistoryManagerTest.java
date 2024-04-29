f544b35d98d816c4cb8973f0f895e72c6cdcdf02
Dmitiy Stepanov <ariorxis@gmail.com>
2024-04-29 16:36:49 +0400

    6 спринт, версия 2. Добавлены тесты для InMemoryHistoryManager.

--- test/com/yandex/sprint4/service/InMemoryHistoryManagerTest.java
+++ test/com/yandex/sprint4/service/InMemoryHistoryManagerTest.java
@@ -2,21 +2,40 @@ package com.yandex.sprint4.service;
 
 import com.yandex.sprint4.model.Task;
 import com.yandex.sprint4.model.TaskStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
-    @Test
-    public void getHistoryTest() {
-        InMemoryHistoryManager inMemoryHistoryManager = new InMemoryHistoryManager();
-        Task task1 = new Task("1", "2", TaskStatus.NEW);
+    InMemoryHistoryManager inMemoryHistoryManager;
+    Task task1;
+    Task task2;
+    @BeforeEach
+    public void beforeEach() {
+        inMemoryHistoryManager = new InMemoryHistoryManager();
+
+        task1 = new Task("1", "2", TaskStatus.NEW);
+        task2 = new Task("2", "3", TaskStatus.NEW);
+
+        task1.setId(1);
+        task1.setId(2);
 
         inMemoryHistoryManager.add(task1);
+        inMemoryHistoryManager.add(task2);
+        inMemoryHistoryManager.add(task1);
+    }
 
-        task1.setName("2");
+    @Test
+    public void getHistoryTest() {
+        assertEquals("2", inMemoryHistoryManager.getHistory().get(0).getName());
+        assertEquals("1", inMemoryHistoryManager.getHistory().get(1).getName());
+    }
 
-        assertEquals("1", inMemoryHistoryManager.getHistory().get(0).getName());
+    @Test
+    public void getHistoryTestAfterRemove() {
+        inMemoryHistoryManager.remove(task1.getId());
+        assertEquals("2", inMemoryHistoryManager.getHistory().get(0).getName());
     }
 
 }
