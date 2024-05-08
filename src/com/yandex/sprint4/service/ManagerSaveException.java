6c677e1e0a8a9c4327d5915ca00204b984bea8c5
Dmitiy Stepanov <ariorxis@gmail.com>
2024-05-08 11:31:23 +0400

    7 спринт, версия 3. Добавлен класс FileBackedTaskManager(наследник InMemoryTaskManager) для сохранения задач в файл. Добавлен класс ManagerSaveException для генерации непроверяемого исключения. Добавлены тесты. Изменены модификаторы доступа мап tasks, epics и subtask в InMemoryTaskManager с private на protected.

--- src/com/yandex/sprint4/service/ManagerSaveException.java
+++ src/com/yandex/sprint4/service/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package com.yandex.sprint4.service;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final Exception e) {
         super(e);
